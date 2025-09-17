15f642070e0afbbb4fe5b04a73bc368058c36450
kesava2601 <chennakesava2601@gmail.com>
2025-09-17 15:25:01 +0000

    testing files

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -34,6 +34,16 @@ def test_area_of_circle_zero_radius():
     assert result == 0
 
 
+def test_area_of_circle_negative_radius():
+   """Test with a negative radius to raise ValueError."""
+   # Arrange
+   radius = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      area_of_circle(radius)
+
+
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
     # Arrange
@@ -45,6 +55,14 @@ def test_get_nth_fibonacci_zero():
     # Assert
     assert result == 0
 
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
 
 def test_get_nth_fibonacci_one():
     """Test with n=1."""
